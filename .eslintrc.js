7108f9ca093bf70f488ac95548863cfbfbfdc5e4
Victor Chen <vlchen91@gmail.com>
2020-11-10 22:33:23 -0500

    Initial run of "prettier --write ."
    Note: I had to add a couple of stuff to .gitignore, .eslintrc.js, and
    .prettierignore to make sure that prettier and eslint dont screw things
    up in existing checked-in code. prettier/eslint should kick in on new
    code submitted after this commit.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
         "es6": true,
         "node": true
     },
-    "extends": "eslint:recommended",
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
-var bodyParser = require('body-parser')
-const routes = require('./routes');
+const express = require("express");
+const dotenv = require("dotenv").config();
+var bodyParser = require("body-parser");
+const routes = require("./routes");
 const app = express();
 const port = process.env.PORT;
 // parse various different custom JSON types as JSON
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(routes);
-app.listen(port, () => console.log(`Quickstart app listening at http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(`Quickstart app listening at http://localhost:${port}`)
+);
 
 module.exports = app;
