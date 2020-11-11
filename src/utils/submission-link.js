b6edd9393fc909134054629ff4ddef5503d42a6e
lrt98802 <rtlin97@gmail.com>
2020-11-10 21:22:32 -0500

    Init functions for generating unique submission link

---
+++ src/utils/submission-link.js
@@ -0,0 +1,21 @@
+import Sha1 from './sha1.js'
+
+function generateSubmissionLink(host, endPoint) {
+  // E.g. 
+  // `host` => https://www.hmwk.herokuapp.com
+  // `endPoint` => /submission
+  // `submissionLink` => https://www.hmwk.herokuapp.com/submission?token=xxxxx
+  const maskedRandomString = generateMaskedRandomString()
+  const submissionLink = `${host}/${endPoint}?token=${maskedRandomString}`
+  return submissionLink
+}
+
+function generateMaskedRandomString() {
+  const base = 36
+  const randomString = Math.random().toString(base).substring(2, 15) + Math.random().toString(base).substring(2, 15);
+  return Sha1.hash(randomString)
+}
+
+module.exports = {
+  generateSubmissionLink
+}
