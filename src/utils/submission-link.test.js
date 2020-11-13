b249d0880f7839b64d0a88c42aabf1a939fb53e3
lrt98802 <rtlin97@gmail.com>
2020-11-12 21:17:51 -0500

    Add unit test

--- src/utils/submission-link.test.js
+++ src/utils/submission-link.test.js
@@ -1,5 +1,19 @@
-const generateSubmissionLink = require("./submission-link");
+const { expect } = require("chai");
+const { generateSubmissionLink } = require("./submission-link");
 
 test("submission link smoke test", () => {
   expect(true);
+});
+
+test("submission link is generated with a fixed prefix", () => {
+  // GIVEN
+  const host = "https://www.hmwk.herokuapp.com";
+  const endPoint = "/submission";
+  const submissionLinkPrefix = `${host}/${endPoint}?token=`;
+
+  // WHEN
+  const submissionLink = generateSubmissionLink(host, endPoint);
+
+  // THEN
+  expect(submissionLink.startsWith(submissionLinkPrefix));
 });
