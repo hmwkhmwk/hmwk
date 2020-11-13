4d8320d506356f217016df8ff15ae47aae13ed46
lrt98802 <rtlin97@gmail.com>
2020-11-12 21:27:48 -0500

    Update test to use expect().equal()

--- src/utils/submission-link.test.js
+++ src/utils/submission-link.test.js
@@ -2,18 +2,18 @@ const { expect } = require("chai");
 const { generateSubmissionLink } = require("./submission-link");
 
 test("submission link smoke test", () => {
-  expect(true);
+  expect(true).equal(true);
 });
 
 test("submission link is generated with a fixed prefix", () => {
   // GIVEN
   const host = "https://www.hmwk.herokuapp.com";
-  const endPoint = "/submission";
+  const endPoint = "submission";
   const submissionLinkPrefix = `${host}/${endPoint}?token=`;
 
   // WHEN
   const submissionLink = generateSubmissionLink(host, endPoint);
 
   // THEN
-  expect(submissionLink.startsWith(submissionLinkPrefix));
+  expect(submissionLink.startsWith(submissionLinkPrefix)).equal(true);
 });
