e2fc4683044d84e3bc088557a9f3923267aca711
Victor Chen <vlchen91@gmail.com>
2020-11-14 20:10:02 -0500

    Implemented the reseeding logic

--- src/scripts/reseed.js
+++ src/scripts/reseed.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 const axios = require("axios").default;
 const { newDB, RESEED_PATH_PREFIX } = require("../db");
 const { DataError } = require("node-json-db/dist/lib/Errors");
+const HmwkService = require("../services/hmwk-service");
+const seed = require("./seed");
 
 async function reseed(subID, sub) {
   console.log(`For subscription ID ${subID}...`);
@@ -16,37 +18,55 @@ async function reseed(subID, sub) {
 
   // Reseed hmwk_assignments.
   console.log(
-    `[dry run] Deleting all items in hmwk_assignments board (id=${hmwkAssignmentsId})`
+    `Deleting all items in hmwk_assignments (id=${hmwkAssignmentsId})`
   );
-  console.log("[dry run] Inserting seed items in hmwk_assignments board");
+  await HmwkService.deleteAllHmwkAssignmentsItems(hmwkAssignmentsId);
+  console.log(`Seeding hmwk_assignments`);
+  await HmwkService.seedHmwkAssignments(
+    hmwkAssignmentsId,
+    seed.assignmentsGroup,
+    seed.assignments
+  );
 
   // Reseed students.
-  console.log(
-    `[dry run] Deleting all items in students board (id=${studentsId})`
-  );
-  console.log("[dry run] Inserting seed items in students board");
+  console.log(`Deleting all items in students board (id=${studentsId})`);
+  await HmwkService.deleteAllStudentsItems(studentsId);
+  console.log("Seeding students");
+  await HmwkService.seedStudents(studentsId, seed.studentsGroup, seed.students);
 
   // Reseed hmwk_completion_tracking.
   console.log(
-    `[dry run] Deleting all items in hmwk_completion_tracking board (id=${hmwkCompletionTrackingId})`
+    `Deleting all items in hmwk_completion_tracking (id=${hmwkCompletionTrackingId})`
   );
-  console.log(
-    "[dry run] Inserting seed items in hmwk_completion_tracking board"
+  await HmwkService.deleteAllHmwkCompletionTrackingItems(
+    hmwkCompletionTrackingId
+  );
+  console.log("Seeding hmwk_completion_tracking");
+  await HmwkService.seedHmwkCompletionTracking(
+    hmwkCompletionTrackingId,
+    seed.assignmentsTracker1Group,
+    seed.assignmentsTracker1
+  );
+  await HmwkService.seedHmwkCompletionTracking(
+    hmwkCompletionTrackingId,
+    seed.assignmentsTracker2Group,
+    seed.assignmentsTracker2
   );
 
   // Notify user.
-  console.log(`[dry run] Sending request to ${webhookUrl}`);
-  const res = await axios.post(
-    webhookUrl,
-    { trigger: {} },
-    {
-      headers: {
-        Authorization: process.env.MONDAY_SIGNING_SECRET,
-      },
-    }
-  );
-  console.log(res.data);
-  console.log(`${res.status} ${res.statusText}`);
+  // TODO(victor): Delete this block of code is it's too spammy.
+  // console.log(`Sending request to ${webhookUrl}`);
+  // const res = await axios.post(
+  //   webhookUrl,
+  //   { trigger: {} },
+  //   {
+  //     headers: {
+  //       Authorization: process.env.MONDAY_SIGNING_SECRET,
+  //     },
+  //   }
+  // );
+  // console.log(res.data);
+  // console.log(`${res.status} ${res.statusText}`);
 }
 
 // We've separated the `reseed` function from the `runReseed` function.
@@ -64,11 +84,16 @@ async function runReseed() {
       } else {
         console.error("Unrecognized error:", err);
       }
-      console.log("Ignoring error and continuing to next subID...");
+      throw err;
     }
     console.log("");
   }
   console.log("Done reseeding.");
+  console.log(
+    'Do not run "npm run reseed" too many times in a short amount of time. ' +
+      "You can get rate-limited by monday.com. " +
+      "See https://monday.com/developers/v2#rate-limits-section for more info."
+  );
 }
 
 // Execute the `reseed` function, IF we ran this module directly (`node seed`).
