fb4e1fa1e938fd4fbd5ba4cfdba5582844b9a2a1
ceciliachung <ceciliayu.chung@gmail.com>
2020-11-22 11:01:28 -0500

    STYLE: some changes in the styling, new logo, etc

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,16 +1,19 @@
 import React from "react";
+// import logo from "";
 import { Link } from "react-router-dom";
 
 const Header = () => (
   <div>
     <div className="header">
-      <div className="headerLogo">
-        <img src="../../public/hmwk_logo_v1.png" alt="hmwk logo" />
+      <img className="logo" src="hmwk_logo.png" alt="hmwk logo" />
+      <div className="headerTitle">
+        <h1>
+          <Link to="/home">Ms. Stacy's Math Class</Link>
+        </h1>
       </div>
-      <h1>
-        <Link to="/home">Ms. Stacy's Math Class</Link>
-      </h1>
-      <h3>6. Multiplication Homework</h3>
+      <div className="headerSubtitle">
+        <h3>6. Multiplication Homework</h3>
+      </div>
     </div>
   </div>
 );
