304b004b9e50c5b73a75bdcaea0784adf1afb373
YuniceXiao <fishxy2@outlook.com>
2020-11-22 14:16:01 -0500

    Feat: AllHmwks and SingleHmwk componnets skeleton set up

--- client/app.js
+++ client/app.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 import { Header, Body, Footer } from "./components";
+import SingleHmwk from "./components/SingleHmwk";
+import AllHmwks from "./components/AllHmwks";
+
 // import Routes from "./routes";
 
 const App = () => {
