d61a205af9f072aa330c6a363f7c3e57a25faec7
ceciliachung <ceciliayu.chung@gmail.com>
2020-11-22 19:29:37 -0500

    FEAT: components linking

--- client/components/Review.js
+++ client/components/Review.js
@@ -1,25 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Review = () => (
-  <div id="body">
-    <div className="greeting">
-      <img
-        src="https://content.mycutegraphics.com/graphics/star/yellow-rounded-corner-star.png"
-        alt="hmwk star"
-      />
+function Review() {
+  return (
+    <div id="body">
+      <div className="greeting">
+        <img
+          src="https://content.mycutegraphics.com/graphics/star/yellow-rounded-corner-star.png"
+          alt="hmwk star"
+        />
+      </div>
+      <div className="prompt">
+        <h1> Well done Tommy! </h1>
+        <h3> You submitted your homework</h3>
+      </div>
+      <div className="imagePreview">
+        {/* Here we will post the preview image(s) of the hmwk submission */}
+      </div>
+      <Link to="/submit">
+        <button
+          type="button"
+          className="buttonCTA"
+          onClick={() => changeHwmk()}
+        >
+          Change it
+        </button>
+      </Link>
+      <Link to="/allHmwks">
+        <div className="cornerButton">See All</div>
+      </Link>
     </div>
-    <div className="prompt">
-      <h1> Well done Tommy! </h1>
-      <h3> You submitted your homework</h3>
-    </div>
-    <div className="imagePreview">
-      {/* Here we will post the preview image(s) of the hmwk submission */}
-    </div>
-    <div className="buttonCTA">
-      <p>Change it</p>
-    </div>
-    <div className="cornerButton">See All</div>
-  </div>
-);
+  );
+}
 
 export default Review;
--- client/components/Submit.js
+++ client/components/Submit.js
@@ -1,24 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Submit = () => (
-  <div id="body">
-    <div className="imagePreview">
-      <img
-        className="imagePreview"
-        src="hmwk_logo.png"
-        alt="Your homework preview"
-      />
+function Submit() {
+  return (
+    <div id="body">
+      <div className="imagePreview">
+        <img
+          className="imagePreview"
+          src="hmwk_logo.png"
+          alt="Your homework preview"
+        />
+      </div>
+      <div>
+        <Link to="/submit">
+          <button type="button" className="addMore">
+            Add More
+          </button>
+        </Link>
+      </div>
+      <div>
+        <Link to="/review">
+          <button type="button" className="buttonCTA">
+            Submit
+          </button>
+        </Link>
+      </div>
     </div>
-
-    <div className="addMore">
-      <p>
-        Add <br></br>More
-      </p>
-    </div>
-    <div className="buttonCTA">
-      <p>Submit</p>
-    </div>
-  </div>
-);
+  );
+}
 
 export default Submit;
