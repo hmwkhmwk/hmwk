2e59a8df8899686d2a8b67bf77e404db5bb32e43
ceciliachung <ceciliayu.chung@gmail.com>
2020-11-24 12:12:56 -0500

    FEAT: API building

---
+++ src/api/hash.js
@@ -0,0 +1,13 @@
+// const router = require("express").Router();
+// // const { ??? } = require("../services/hmwk-service");
+
+// router.get("/hash", async (req, res, next) => {
+//     try {
+//       const hash = await ???();
+//       res.json(hash);
+//     } catch (error) {
+//       next(error);
+//     }
+//   });
+
+// module.exports = router;
---
+++ src/api/index.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+module.exports = router;
+
+router.use("/student", require("./student"));
+router.use("/homework", require("./homework"));
+router.use("/homeworks", require("./homeworks"));
+router.use("/hash", require("./hash"));
+
+router.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
